a0f155bd0b2ec6521f2a00aa31020e636117bac5
Alexandra Ward <amw349@cornell.edu>
2017-07-14 15:27:41 -0400

    Routes separated into json's with routes.json containing all of them. Added AllRoutes component that draws all routes on the map.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Install
 
 ```bash
+git clone https://github.com/amw349/praemoveo.git
+cd praemoveo
 yarn
 cd ios
 pod install
---
+++ src/components/AllRoutes.js
@@ -0,0 +1,58 @@
+/**
+ * Created by alexandraward on 7/14/17.
+ */
+/**
+ * Created by alexandraward on 7/11/17.
+ */
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    Animated
+} from 'react-native';
+import MapView from 'react-native-maps';
+import routes from '../routes/routes.json';
+
+export default class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            routes: routes.routes,
+            strokeWidth: 2,
+        }
+    }
+
+    renderRoutes() {
+        var routes = [];
+        for (i in this.state.routes) {
+            routes[i] = <MapView.Polyline
+                coordinates={this.state.routes[i].geometry.coordinates}
+                strokeColor={this.state.routes[i].properties.color}
+                strokeWidth={this.state.strokeWidth}
+            />;
+        }
+        return routes;
+    }
+
+    render() {
+        let renderRoutes = this.renderRoutes();
+        return (
+            <View style={styles.container}>
+                {renderRoutes}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+AppRegistry.registerComponent('AllRoutes', () => AllRoutes);
