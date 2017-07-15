41133a20d0e93ac145cb4963e836b2835325529c
Edrick Ortiz <edrick.ortiz@gmail.com>
2017-07-15 14:41:23 -0400

    - fix issue

--- src/components/AllRoutesCaguas/AllRoutesCaguas.js
+++ src/components/AllRoutesCaguas/AllRoutesCaguas.js
@@ -23,33 +23,23 @@ export default class AllRoutesCaguas extends Component {
     }
 
     renderRoutes() {
-        var routes = [];
-        for (i in this.state.routes) {
-            routes[i] = <MapView.Polyline
-                coordinates={this.state.routes[i].geometry.coordinates}
-                strokeColor={this.state.routes[i].properties.color}
+        return this.state.routes.map((element) =>
+            <MapView.Polyline
+                coordinates={element.geometry.coordinates}
+                strokeColor={element.properties.color}
                 strokeWidth={this.state.strokeWidth}
-            />;
-        }
-        return routes;
+            />
+        );
     }
 
     render() {
         let renderRoutes = this.renderRoutes();
         return (
-            <View style={styles.container}>
+            <View>
                 {renderRoutes}
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-});
 
 AppRegistry.registerComponent('AllRoutesCaguas', () => AllRoutesCaguas);
